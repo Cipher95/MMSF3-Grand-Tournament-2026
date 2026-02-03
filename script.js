99d627a106142bdd64ab32ab2672728efcb01daf
Cipher95 <h.alobaidan95@gmail.com>
2026-02-03 22:05:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 { rank: 0, name: 'WolZardFire!', mp: 0, w: 0, d: 0, l: 0, diff: 0, pts: 0, sf: 0 }
             ],
             matches: [
-               /* { p1: 'Baguette', s1: 0, p2: 'Ryuken', s2: 0 },
-                { p1: 'Cipher', s1: 0, p2: 'WolZardFire!', s2: 0 },
-                { p1: 'Baguette', s1: 0, p2: 'Cipher', s2: 0 },
-                { p1: 'Ryuken', s1: 0, p2: 'WolZardFire!', s2: 0 },
+               /* { p1: 'Baguette', s1: 0, p2: 'Ryuken', s2: 0 },*/
+                { p1: 'Cipher', s1: 2, p2: 'WolZardFire!', s2: 0 }/*,
+                { p1: 'Baguette', s1: 0, p2: 'Cipher', s2: 0 },*/
+                /*{ p1: 'Ryuken', s1: 0, p2: 'WolZardFire!', s2: 0 },
                 { p1: 'Baguette', s1: 0, p2: 'WolZardFire!', s2: 0 },
 		{ p1: 'Ryuken', s1: 0, p2: 'Cipher', s2: 0 } */
             ]
@@ -94,10 +94,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 { rank: 0, name: 'WolZardFire!', mp: 0, w: 0, d: 0, l: 0, diff: 0, pts: 0, sf: 0 }
             ],
             matches: [
-               /* { p1: 'Baguette', s1: 0, p2: 'Ryuken', s2: 0 },
-                { p1: 'Cipher', s1: 0, p2: 'WolZardFire!', s2: 0 },
-                { p1: 'Baguette', s1: 0, p2: 'Cipher', s2: 0 },
-                { p1: 'Ryuken', s1: 0, p2: 'WolZardFire!', s2: 0 },
+               /* { p1: 'Baguette', s1: 0, p2: 'Ryuken', s2: 0 },*/
+                { p1: 'Cipher', s1: 2, p2: 'WolZardFire!', s2: 0 }/*,
+                { p1: 'Baguette', s1: 0, p2: 'Cipher', s2: 0 },*/
+                /*{ p1: 'Ryuken', s1: 0, p2: 'WolZardFire!', s2: 0 },
                 { p1: 'Baguette', s1: 0, p2: 'WolZardFire!', s2: 0 },
 		{ p1: 'Ryuken', s1: 0, p2: 'Cipher', s2: 0 } */
             ]
